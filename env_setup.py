6b88a794ef60cf2a0a8d1757bd006ef2d7401702
FelipeLoureiroQA <felipe.desenvolvedor@hotmail.com.br>
2024-07-15 12:17:55 -0300

    fix: edit steps

--- env_setup.py
+++ env_setup.py
@@ -3,12 +3,14 @@ import os
 
 def load_env_vars():
     load_dotenv()
+    
+os.environ["BEARER_TOKEN"]
 
-    env_vars = {
-        'BEARER_TOKEN': os.environ('BEARER_TOKEN'),
-    }
+#     env_vars = {
+#         'BEARER_TOKEN': os.getenv('BEARER_TOKEN'),
+#     }
 
-    return env_vars
+#     return env_vars
 
-if __name__ == "__main__":
-    load_env_vars()
+# if __name__ == "__main__":
+#     load_env_vars()
